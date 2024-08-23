e41c9bafe41bb9beab31a9cf9277723afd85b610
Giovani Silva <giovanibrolyy2016@gmail.com>
2024-08-22 22:02:44 -0300

    adicionando validação de quantidade

--- js/app.js
+++ js/app.js
@@ -2,10 +2,14 @@ let totalGeral
 limpar()
 
 function adicionar() {
+
     let produto = document.getElementById('produto').value
     let nomeProduto = produto.split('-')[0]
     let valorUnitario = produto.split('R$')[1]
     let quantidade = document.getElementById('quantidade').value
+    if (quantidade < 1) {
+        return alert('Selecione a quantidade desejada')
+    }
     let preco = quantidade * valorUnitario
     let carrinho = document.getElementById('lista-produtos')
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
